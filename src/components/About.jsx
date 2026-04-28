79a0dac5c50fcdf2f801d3cd87cac858bbf4fc57
mylesmunroe <munroemil6@gmail.com>
2026-04-28 23:13:08 +0300

    test confirmation and criteria met

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,13 @@
-function About({ image, about }) {
+const About = ({
+  image = "https://via.placeholder.com/215",
+  about = ""
+}) => {
   return (
     <aside>
       <img src={image} alt="blog logo" />
       <p>{about}</p>
     </aside>
   );
-}
+};
 
 export default About;
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,4 +1,8 @@
-function Article({ title, date, preview }) {
+function Article({
+  title,
+  date = "January 1, 1970",
+  preview
+}) {
   return (
     <article>
       <h3>{title}</h3>
