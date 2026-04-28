425c9bdcf7ac5a8694cd431856fdbf7593db0b19
mylesmunroe <munroemil6@gmail.com>
2026-04-28 22:38:50 +0300

    connecting components via the app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./components/Header";
+import About from "./components/About";
+import ArticleList from "./components/ArticleList";
+import blog from "./blog";
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div>
+      <Header name={blog.name} />
+      <About image={blog.image} about={blog.about} />
+      <ArticleList posts={blog.posts} />
     </div>
   );
 }
